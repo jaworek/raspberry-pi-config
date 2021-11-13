7294d611e0fa472b4cd19db0eabbc088b36fc8ae
Jan Jaworski <jaworek3211@gmail.com>
2021-11-13 22:02:59 +0100

    Update start-docker.sh

--- start-docker.sh
+++ start-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker-compose up -d --remove-orphans -f ./adguard.service.yml ./portainer.service.yml ./
+docker-compose up -d --remove-orphans -f ./traefik.service.yml ./adguard.service.yml ./portainer.service.yml
